04e43d1deeb67e119e2cfc58d02e2daf4d7a748b
ZhaoChongran <imchongran@gmail.com>
2024-06-02 17:07:04 +0800

    need to add multi-releaxation process

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clc; clear; close all
 
-fid = fopen('data_1.txt', 'r');
+fid = fopen('data_0d6.txt', 'r');
 data = textscan(fid, '%f %f %f', 'Delimiter', '\n');
 fclose(fid);
 
@@ -13,75 +13,104 @@ Ft(1,1,:) = strain(:);
 Ft(2,2,:) = (strain(:)).^(-0.5);
 Ft(3,3,:) = (strain(:)).^(-0.5);
 
-% xi_eq = [-0.002159592828186, -2.459522226986577, 0.010982802848101, 4.018808768072496, 6.547826606246044, 0.112778494893808];
-% xi_eq =  [1.0, 1.0, 1.0, 1.0, -1.0, -1.0];
-xi_neq = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
+xi_eq_0 =  [2.0, 2.0];
+xi_neq_0 = [1.0, 2.0];
+eta_0 = 1;
 
-be = get_be(time, xi_neq, Ft);
-% disp(be);
-% paras0 = [xi_eq, xi_neq];
-% objectiveFunction = @(paras) objective(paras, Ft, stress, time);
-% options = optimoptions('lsqnonlin', 'Algorithm', 'interior-point', 'MaxIterations', 5000);
-% % lb = [0.0, 0.0, 0.0, 0.0, -Inf, -Inf];
-% % ub = [Inf, Inf, Inf, Inf,  0.0,  0.0];
-% [paras, ~] = lsqnonlin( objectiveFunction, paras0, [], [], options);
-% P1_eq_list = get_P1_eq_list(paras, Ft);
-% 
-% plot(Treloar_UT_strain, Treloar_UT_stress, 'o', Color='r');
-% hold on
-% plot(Treloar_UT_strain, P1_eq_list, '-', Color='r');
+paras0 = [xi_eq_0, xi_neq_0, eta_0];
+objectiveFunction = @(paras) objective(paras, Ft, stress, time);
+options = optimoptions('lsqnonlin', 'Algorithm', 'interior-point', 'MaxIterations', 5000);
+% lb = [ 0.0, 0.0, 0.0, 0.0, -Inf, -Inf, -Inf, -Inf, -Inf, -Inf, -Inf, -Inf, 0.0];
+% ub = [ Inf, Inf, Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf, Inf];
+[paras, ~] = lsqnonlin( objectiveFunction, paras0, [], [], options);
+xi_eq = paras(1:2)
+xi_neq = paras(3:4)
+eta = paras(end)
+be = get_be(time, xi_neq, eta, Ft);
+P1_list = get_P1_list(xi_eq, xi_neq, Ft, be);
+P1_eq_list = get_P1_eq_list(xi_eq, Ft);
 
+plot(strain, stress, 'o', Color='r');
+hold on
+plot(strain, P1_list, '-', Color='r');
+
+% xi_eq is 2x1 array
+% lambda is a scalar
 function out = eq_dPsi_dlambda(xi_eq, lambda)
-
-out = xi_eq(1) .* lambda.^( xi_eq(2)-1.0 )...
-    + xi_eq(3) .* lambda.^( xi_eq(4)-1.0 )...
-    + xi_eq(5) .* lambda.^( xi_eq(6)-1.0 );
+out = xi_eq(1) * lambda^( xi_eq(2)-1.0 );
 end
 
+% xi_neq is a 2x1 array
+% lambda is a scalar
 function out = neq_dPsi_dlambda(xi_neq, lambda)
-
-out = xi_neq(1) .* lambda.^( xi_neq(2)-1.0 )...
-    + xi_neq(3) .* lambda.^( xi_neq(4)-1.0 )...
-    + xi_neq(5) .* lambda.^( xi_neq(6)-1.0 );
+out = xi_neq(1) .* lambda.^( xi_neq(2)-1.0 );
 end
 
+% xi_eq is a 6x1 array
+% F is a 3x3 matrix
+% out is a 3x3 matrix
 function out = get_S_eq(xi_eq, F)
-out = zeros(3,3);
 C = transpose(F)*F;
 [V, D] = eig(C);
-N1 = V(:,1);
-N2 = V(:,2);
-N3 = V(:,3);
 lambda1 = sqrt(D(1,1));
 lambda2 = sqrt(D(2,2));
 lambda3 = sqrt(D(3,3));
-out = out + eq_dPsi_dlambda(xi_eq, lambda1) ./ lambda1 .* kron(N1, N1') ;
-out = out + eq_dPsi_dlambda(xi_eq, lambda2) ./ lambda2 .* kron(N2, N2');
-out = out + eq_dPsi_dlambda(xi_eq, lambda3) ./ lambda3 .* kron(N3, N3');
+
+eig_val_S_eq = [eq_dPsi_dlambda(xi_eq, lambda1) / lambda1;...
+                eq_dPsi_dlambda(xi_eq, lambda2) / lambda2;...
+                eq_dPsi_dlambda(xi_eq, lambda3) / lambda3];
+out = tensor_product(V, eig_val_S_eq);
 end
 
-function out = get_S_neq(xi_neq, be)
-out = zeros(3,3);
+% xi_neq is a 6x1 array
+% F is a 3x3 matrix
+% out is a 3x3 matrix
+function out = get_S_neq(xi_neq, be, F)
+out = inv(F) * get_tau_neq(xi_neq, be) * inv(transpose(F));
+end
+
+% xi_neq is a 6x1 array
+% eig_val_be is a 1x3 vector
+% out is a 1x3 vector
+function out = get_eig_val_tau_neq(xi_neq, eig_val_be)
+out = zeros(3,1);
+mu_neq = [xi_neq(1)];
+alpha_neq = [xi_neq(2)];
+coe1 = 2.0 / 3.0;
+coe2 = -1.0 / 3.0;
+for ii = 1:length(mu_neq)
+    out(1) = out(1) + mu_neq(ii)*(coe1*eig_val_be(1)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(2)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(3)^(0.5*alpha_neq(ii)));
+    out(2) = out(2) + mu_neq(ii)*(coe1*eig_val_be(2)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(1)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(3)^(0.5*alpha_neq(ii)));
+    out(3) = out(3) + mu_neq(ii)*(coe1*eig_val_be(3)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(1)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(2)^(0.5*alpha_neq(ii)));
+end
+end
+
+function out = get_tau_neq(xi_neq, be)
 [V, D] = eig(be);
-N1 = V(:,1);
-N2 = V(:,2);
-N3 = V(:,3);
-lambda1 = sqrt(D(1,1));
-lambda2 = sqrt(D(2,2));
-lambda3 = sqrt(D(3,3));
-out = out + neq_dPsi_dlambda(xi_neq, lambda1) ./ lambda1 .* kron(N1, N1') ;
-out = out + neq_dPsi_dlambda(xi_neq, lambda2) ./ lambda2 .* kron(N2, N2');
-out = out + neq_dPsi_dlambda(xi_neq, lambda3) ./ lambda3 .* kron(N3, N3');
+eig_val_be = [D(1,1); D(2,2); D(3,3)];
+eig_val_tau_neq = get_eig_val_tau_neq(xi_neq, eig_val_be);
+out = tensor_product(V, eig_val_tau_neq);
 end
 
+% xi_eq is a 6x1 array
+% F is a 3x3 matrix
+% out is a 3x3 matrix
 function out = get_P_eq(xi_eq, F)
 out = F * get_S_eq(xi_eq, F);
+% disp(out);
 end
 
+% xi_neq is a 6x1 array
+% F is a 3x3 matrix
+% be is a 3x3 matrix
+% out is a 3x3 matrix
 function out = get_P_neq(xi_neq, F, be)
-out = F * get_S_neq(xi_neq, be);
+out = F * get_S_neq(xi_neq, be, F);
 end
 
+% xi_eq is a 6x1 array
+% Ft is size of (3, 3, length of data)
+% out is the same size of Ft
 function out = get_P_eq_list(xi_eq, Ft)
 out = zeros(size(Ft));
 for ii = 1:size(Ft, 3)
@@ -89,18 +118,58 @@ for ii = 1:size(Ft, 3)
 end
 end
 
-function out = get_P_neq_list(xi_neq, Ft, be)
+% xi_neq is a 6x1 array
+% Ft is size of (3, 3, length of data)
+% be_t is the same size of Ft
+% out is the same size of Ft
+function out = get_P_neq_list(xi_neq, Ft, be_t)
 out = zeros(size(Ft));
 for ii=1:size(Ft, 3)
-out(:,:,ii) = F * get_P_neq(xi_neq, Ft(:,:,ii), be(:,:,ii));
+out(:,:,ii) = F * get_P_neq(xi_neq, Ft(:,:,ii), be_t(:,:,ii));
 end
 end
 
+% The function is used to extract the first pricipal
+% value of equilibrium part of first PK stress
+% xi_eq is a 6x1 array
+% Ft is size of (3, 3, length of data)
+% out is an array of length of data
+function out = get_P1_eq_list(xi_eq, Ft)
+out = zeros(size(Ft, 3), 1);
+P_eq_list = get_P_eq_list(xi_eq, Ft);
+for ii = 1:length(out)
+    out(ii) = P_eq_list(1, 1, ii);
+end
+end
+
+% The function is used to extract the first pricipal
+% value of non-equilibrium part of first PK stress
+% xi_neq is a 6x1 array
+% Ft is size of (3, 3, length of data)
+% be_t is same size of Ft
+% out is an array of length of data
+function out = get_P1_neq_list(xi_neq, Ft, be_t)
+out = zeros(size(Ft, 3), 1);
+P_neq_list = get_P_neq_list(xi_neq, Ft, be_t);
+for ii = 1:length(out)
+    out(ii) = P_neq_list(1, 1, ii);
+end
+end
+
+% The function is used to get the inverse Cv based
+% on the multiplicative decomposition
+% refer to eqn. (10) of Reese & Govindjee 1998 IJSS
+% F is a 3x3 matrix
+% be is a 3x3 matrix
 function out = get_Cv_inv(F, be)
 out = inv(F) * be * inv(transpose(F));
 end
 
-function out = get_be(time, xi_neq, Ft)
+% Ft is size of (3, 3, length of data)
+% xi_neq is 6x1 array
+% time is size of length of data
+% out is same size of Ft
+function out = get_be(time, xi_neq, eta, Ft)
 out = zeros(size(Ft));
 for ii = 1:size(out, 3)
     if (ii==1)
@@ -112,8 +181,6 @@ for ii = 1:size(out, 3)
     end
     
     be_trial = Ft(:,:,ii) * Cv_old * transpose(Ft(:,:,ii));
-    be = be_trial;
-    eta = xi_neq(end);
     error = 1.0;
     tol = 1e-5;
     max_it_num = 1000;
@@ -142,38 +209,28 @@ for ii = 1:size(out, 3)
 end
 end
 
-function out = get_eig_val_tau_neq(xi_neq, eig_val_be)
-out = zeros(3,1);
-mu_neq = [xi_neq(1), xi_neq(3), xi_neq(5)];
-alpha_neq = [xi_neq(2), xi_neq(4), xi_neq(6)];
-coe1 = 2.0 / 3.0;
-coe2 = -1.0 / 3.0;
-for ii = 1:length(mu_neq)
-    out(1) = out(1) + mu_neq(ii)*(coe1*eig_val_be(1)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(2)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(3)^(0.5*alpha_neq(ii)));
-    out(2) = out(2) + mu_neq(ii)*(coe1*eig_val_be(2)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(1)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(3)^(0.5*alpha_neq(ii)));
-    out(3) = out(3) + mu_neq(ii)*(coe1*eig_val_be(3)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(1)^(0.5*alpha_neq(ii)) + coe2*eig_val_be(2)^(0.5*alpha_neq(ii)));
-end
-end
 
-function out = get_epsilon(be)
-out = zeros(3, 3);
-[V, D] = eig(be);
-out = out + 0.5 * D(1,1) .* kron(V(:,1), V(:,1)');
-out = out + 0.5 * D(2,2) .* kron(V(:,2), V(:,2)');
-out = out + 0.5 * D(3,3) .* kron(V(:,3), V(:,3)');
-end
-
+% all inputs are 1x3 eigenvalue vectors
+% xi_neq is a 6x1 array
+% dt is scalar
+% eta is scalar
+% out is a 1x3 vector
 function out = get_res(eig_val_be, eig_val_eps, eig_val_eps_trial, xi_neq, dt, eta)
 out = eig_val_eps + dt .* 0.5 ./ eta .* get_eig_val_tau_neq(xi_neq, eig_val_be) - eig_val_eps_trial;
 end
 
+% eig_val_be is a 1x3 vector
+% xi_neq is a 6x1 array
+% dt is a scalar
+% eta is a scalar
+% out is a 3x3 matrix
 function out = get_res_tangent(eig_val_be, xi_neq, dt, eta)
 out = zeros(3,3);
 coe1 = -2.0 / 9.0;
 coe2 = 1.0 / 9.0;
 coe3 = 4.0 / 9.0;
-mu_neq = [xi_neq(1), xi_neq(3), xi_neq(5)];
-alpha_neq = [xi_neq(2), xi_neq(4), xi_neq(6)];
+mu_neq = [xi_neq(1)];
+alpha_neq = [xi_neq(2)];
 
 for ii = 1:length(mu_neq)
     out(1,2) = out(1,2) + mu_neq(ii) * alpha_neq(ii) * (coe1 * (eig_val_be(1)^(0.5*alpha_neq(ii))) + coe1 * (eig_val_be(2)^(0.5*alpha_neq(ii))) + coe2 * (eig_val_be(3)^(0.5*alpha_neq(ii))));
@@ -192,6 +249,8 @@ out(2,2) = out(2,2) + 1.0;
 out(3,3) = out(3,3) + 1.0;
 end
 
+% D is a 1x3 vector containing three eigenvalues
+% V is a 3x3 matrix containing three cooresponidng eigenvectors
 function out = tensor_product(V, D)
 out = zeros(3,3);
 out = out + D(1) .* kron(V(:,1), V(:,1)');
@@ -199,13 +258,29 @@ out = out + D(2) .* kron(V(:,2), V(:,2)');
 out = out + D(3) .* kron(V(:,3), V(:,3)');
 end
 
-function out = solve_p_list(xi_eq, xi_neq, Ft, be)
+% The function is used to solve the pressure p based
+% on the incompressibility constrain of each time step
+% both xi_eq and xi_neq are 6x1 arrays
+% Ft is size of (3, 3, length of data)
+% be is size of (3, 3, length of data)
+% out is a array with length of data
+function out = solve_p_list(xi_eq, xi_neq, Ft, be_t)
 out = zeros(size(Ft, 3), 1);
 for ii=1:length(out)
-    C_inv = inv(transpose(Ft(:,:,ii)) * Ft(:,:,ii));
-    S_eq = get_S_eq(xi_eq, Ft(:,:,ii));
-    S_neq = get_S_neq(xi_neq, be(:,:,ii));
-    out(ii) = (S_eq(2,2) + S_neq(2,2)) / C_inv(2,2);
+    F_inv_transpose = inv(transpose(Ft(:,:,ii)));
+    P_eq = get_P_eq(xi_eq, Ft(:,:,ii));
+    P_neq = get_P_neq(xi_neq, Ft(:,:,ii), be_t(:,:,ii));
+    out(ii) = (P_eq(2,2) + P_neq(2,2)) / F_inv_transpose(2,2);
+    if abs((P_eq(2,2) + P_neq(2,2)) / F_inv_transpose(2,2) - (P_eq(3,3) + P_neq(3,3)) / F_inv_transpose(3,3)) < 1e-6
+        out(ii) = (P_eq(2,2) + P_neq(2,2)) / F_inv_transpose(2,2);
+    else
+        format long
+        disp(xi_neq);
+        disp(F_inv_transpose);
+        disp((P_eq(3,3) + P_neq(3,3)) / F_inv_transpose(3,3));
+        disp(get_tau_neq(xi_neq,be_t(:,:,ii)));
+        error("ERROR: solve_p_list is wrong!");
+    end
 end
 end
 
@@ -215,16 +290,23 @@ p = solve_p_list(xi_eq, xi_neq, Ft, be);
 for ii=1:length(out)
     P_eq = get_P_eq(xi_eq, Ft(:,:,ii));
     P_neq = get_P_neq(xi_neq, Ft(:,:,ii), be(:,:,ii));
-    out(ii) = out(ii) + P_eq(1,1) + P_neq(1,1) - p(ii);
+    F_inv_transpose = inv(transpose(Ft(:,:,ii)));
+    out(ii) = out(ii) + P_eq(1,1) + P_neq(1,1) - p(ii)*F_inv_transpose(1,1);
 end
 end
 
 function out = objective(paras, Ft, time, P1_exp)
-xi_eq = paras(1:6);
-xi_neq = paras(1:end);
-be = get_be(time, xi_neq, Ft);
+xi_eq = paras(1:2);
+xi_neq = paras(3:4);
+eta = paras(end);
+be = get_be(time, xi_neq, eta, Ft);
 P1_list = get_P1_list(xi_eq, xi_neq, Ft, be);
-out = 1.0 / length(P1_exp) .* sum( (P1_list - P1_exp).^2 );
+% out = zeros(length(time), 1);
+% for ii=1:length(out)
+out = abs(P1_list - P1_exp)
+% end
+disp(out);
+
 end
 
 % generate the fourth-order viscosity tensor
--- test.m
+++ test.m
@@ -1,19 +1,17 @@
 clc; clear; close all
-fid = fopen('data_1.txt', 'r');
-data = textscan(fid, '%f %f %f', 'Delimiter', '\n');
-fclose(fid);
+% xi_eq =  [1.0, 1.0, 1.0, 1.0, -1.0, -1.0];
+xi_eq = [1.948379758734208   0.001038874981972   1.948466536801018   0.001038830914139  -0.001359939450977  -1.780081771148665
+];
+lambda1 = 1.239113200000000;
+lambda2 = 0.898347799373234;
+aa = eq_dPsi_dlambda(xi_eq, lambda1) / lambda1
+bb = eq_dPsi_dlambda(xi_eq, lambda2) / lambda2
+function out = eq_dPsi_dlambda(xi_eq, lambda)
 
-stress = data{1}';
-strain = data{2}';
-time = data{3}';
-
-Ft = zeros(3,3,length(strain));
-Ft(1,1,:) = strain(:);
-Ft(2,2,:) = (strain(:)).^(-0.5);
-Ft(3,3,:) = (strain(:)).^(-0.5);
-
-xi_neq = [1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0];
-be = get_be(time, xi_neq, Ft);
+out = xi_eq(1) * lambda^( xi_eq(2)-1.0 )...
+    + xi_eq(3) * lambda^( xi_eq(4)-1.0 )...
+    + xi_eq(5) * lambda^( xi_eq(6)-1.0 );
+end
 
 
 
